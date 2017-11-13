2906f598ea4563eb4553d53b94e7484a88d22cde
Dillon <dugan_dillon@yahoo.com>
2017-11-13 14:56:21 -0700

    02

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import Header from "./Header";
+import Home from "./Home";
+
+const mapStateToProps = state => ({
+  appName: state.appName
+});
+
+class App extends Component {
+  state = {};
+
+  render() {
+    const appName = this.props.appName;
+    return (
+      <div>
+        <Header appName={appName} />
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(App);
